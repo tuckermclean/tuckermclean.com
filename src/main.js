4722fc2eb50d1a9f9a34d596ab0c354770a03f7c
Tucker McLean <me@tuckermclean.com>
2025-05-25 17:53:49 -0500

    Swap out page title with identity for domain

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import "@fontsource/fira-code";
 import "@fontsource/fira-sans";
 import './style.css'
 import { openPage, toggleMode, createWindow, cascadeWindows, tileWindows, minimizeWindows } from './window.js'
+import { envVars } from './env.js'
+
+envVars(false).then((ENV_VARS) => {
+    // Replace the text in the page title with the name (using regex)
+    const pageTitle = document.querySelector('head title');
+    pageTitle.textContent = pageTitle.textContent.replace('Tucker McLean', ENV_VARS.NAME);
+});
 
 // Remove class 'not-loaded' from html
 document.documentElement.classList.remove('not-loaded')
--- writings/assets/js/logo_text.js
+++ writings/assets/js/logo_text.js
@@ -6,6 +6,9 @@ const logo = document.querySelector('.logo');
 // Select the link in the copyright footer
 const copyrightLink = document.querySelector('.copyright a');
 
+// Select the page title from the head
+const pageTitle = document.querySelector('head title');
+
 // Then, get the domain name
 const domain = window.location.hostname;
 
@@ -14,10 +17,14 @@ switch (domain) {
     case 'alijamaluddin.com':
         logo.textContent = 'Ali Jamaluddin - Writings';
         copyrightLink.textContent = 'Ali Jamaluddin';
+        // Replace "Tucker McLean" in the page title with "Ali Jamaluddin"
+        pageTitle.textContent = pageTitle.textContent.replace('Tucker McLean', 'Ali Jamaluddin');
         break;
     case 'technomantics.com':
         logo.textContent = 'Developer McDev - Writings';
         copyrightLink.textContent = 'Developer McDev';
+        // Replace "Tucker McLean" in the page title with "Developer McDev"
+        pageTitle.textContent = pageTitle.textContent.replace('Tucker McLean', 'Developer McDev');
         break;
     default:
         logo.textContent = 'Tucker McLean - Writings';
